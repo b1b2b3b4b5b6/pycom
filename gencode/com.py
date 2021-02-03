3e7a4511692f11f77ee905f753627fa3601a7c41
b1b2b3b4b5b6 <a1439458305@163.com>
2021-02-03 14:19:54 +0800

    add gen

--- gencode/com.py
+++ gencode/com.py
@@ -1,13 +1,14 @@
 '''
 Author: your name
 Date: 2020-11-25 14:25:10
-LastEditTime: 2020-12-09 16:16:08
+LastEditTime: 2021-02-03 14:19:26
 LastEditors: Please set LastEditors
 Description: In User Settings Edit
 FilePath: \codegen\common\gencode\com.py
 '''
 
 import time
+import common.tool.codsty as codsty
 
 
 def gen_datetime(config, format='default'):
@@ -18,3 +19,11 @@ def gen_datetime(config, format='default'):
 
 def gen_upper(config, in_str=''):
     return in_str.upper()
+
+
+def gen_lower(config, in_str=''):
+    return in_str.lower()
+
+
+def gen_uc(config, in_str=''):
+    return codsty.sc2ucc(in_str)
